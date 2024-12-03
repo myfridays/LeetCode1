03aed97e94ea661406897f276b59ac76790008ca
myfridays <hyh_myfridays@163.com>
2024-12-03 19:07:20 +0800

    add

--- 数组&字符串/合并两个有序数组.cpp
+++ 数组&字符串/合并两个有序数组.cpp
@@ -65,3 +65,20 @@ public:
         return;
     }
 };
+
+
+
+/**
+ * 第三种方法
+ * 思路： 先合并，在排序
+ * 复杂度：O((m+n)log(m+n))
+ **/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
