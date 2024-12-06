fa68ead7293c6d1522f6b23c4fabe7b222d4d13e
myfridays <hyh_myfridays@163.com>
2024-12-06 17:58:21 +0800

    add

--- 数组&字符串/238.除自身以外数组的乘积.cpp
+++ 数组&字符串/238.除自身以外数组的乘积.cpp
@@ -27,3 +27,31 @@ public:
         return nums;
     }
 };
+
+
+/*法二
+*求前缀和后缀数组，最后乘积为前缀乘以后缀
+*时间复杂度：O(n); 空间复杂度O(n)
+*/
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> L(n, 0), R(n, 0); // 前后缀数组
+        vector<int> ans(n, 0); // 结果
+        L[0] = 1;
+        for(int i = 1; i < n; i++){
+            L[i] = L[i-1] * nums[i-1]; // 前缀数组
+        }
+
+        R[n-1] = 1;
+        for(int i = n-2; i >= 0; i--){
+            R[i] = R[i+1] * nums[i+1]; // 后缀数组
+        }
+
+        for(int i = 0; i < n; i++){
+            ans[i] = L[i] * R[i];
+        }
+        return ans;
+    }
+};
