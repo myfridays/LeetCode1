e39daf57c4a89b1901ccce0805d62ff2b3bd3af7
myfridays <hyh_myfridays@163.com>
2024-12-09 12:36:12 +0800

    update

--- 数组&字符串/14.最长公共前缀.cpp
+++ 数组&字符串/14.最长公共前缀.cpp
@@ -30,3 +30,23 @@ public:
         return str;
     }
 };
+
+
+//题友：直接字符串排序然后对比第一个和最后一个的前缀
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        sort(strs.begin(), strs.end());
+        string ans = "";
+        for(int i = 0; i < strs[0].size() && i < strs[n - 1].size(); ++i){
+            if(strs[0][i] == strs[n - 1][i]){
+                ans += strs[0][i];
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
